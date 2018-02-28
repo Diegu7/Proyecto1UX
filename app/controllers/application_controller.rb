b97cd1047b071b7c59f0813e30be52d4206941ad
Diegu Montes <mecayounbloque@gmail.com>
2018-02-28 17:00:46 -0600

    fixed signup forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
+  before_action :authenticate_user! 
+
+  before_action :configure_permitted_params, if: :devise_controller?
 
   protected
-  	def configure_permitted_parameters
+  	def configure_permitted_params
   		devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :is_trainer])
   		devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     end
