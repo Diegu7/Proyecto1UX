45a8fbc990b3e0ddc793ee2d99401f2422d97300
Ivan Carcamo <ivandres73@unitec.edu>
2018-02-28 18:46:27 -0800

    rutas de messages puestas y creado form de messages (no funca)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,16 @@
 class MessagesController < ApplicationController
+	def create
+		@chat_room = ChatRoom.find(params[:chat_room_id])
+		@message = @chat_room.message.create(message_params)
+		@message.user_id = current_user.id if current_user
+		@message.save
+   
+   		redirect_to chat_room_path(@chat_room)
+    end
+
+    private
+    def message_params
+    	params.require(:message).permit(:body, :user_id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :chat_rooms, only: [:new, :create, :show, :index]
+  resources :chat_rooms, only: [:new, :create, :show, :index] do
+    resources :messages
+  end
   
   root 'chat_rooms#index'
 end
